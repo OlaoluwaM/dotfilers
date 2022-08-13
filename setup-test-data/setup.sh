10feb41f59b4567fac37ce0fb14916e5a638053b
OlaoluwaM <37044906+OlaoluwaM@users.noreply.github.com>
2022-08-13 03:29:27 +0100

    chore: set up script for creating test env and config for testing

--- setup-test-data/setup.sh
+++ setup-test-data/setup.sh
@@ -1,15 +1,27 @@
 #!/usr/bin/env bash
 
 rootDir=$(dirname "$(realpath "$0")")
-DATA_DIR="$(dirname "$rootDir")/tests/mock-env"
+DATA_DIR="$(dirname "$rootDir")/tests/test-data"
 
 test -d "$DATA_DIR" && rm -rf "$DATA_DIR"
 
-echo "Seting up mock-env directory...."
+echo "Seting up test-data directory...."
 mkdir "$DATA_DIR"
 
-echo "Creating mock-dots directory..."
-mkdir -p "$DATA_DIR/mock-dots"
+echo "Creating test data directory for testing link command..."
+mkdir -p "$DATA_DIR/link/mock-dots"
+mkdir -p "$DATA_DIR/link/mock-home"
+echo -e "Done!\n"
 
-echo "Creating mock-home directory..."
-mkdir -p "$DATA_DIR/mock-home"
+echo "Creating test data directory for learning tests..."
+
+echo "For globby..."
+mkdir -p "$DATA_DIR/learning/globby"
+touch $DATA_DIR/learning/globby/{sample.ts,cat.txt,sample.rs,test.js,destinations.json}
+
+mkdir -p "$DATA_DIR/learning/globby/inner"
+touch $DATA_DIR/learning/globby/inner/{sample.ts,bat.txt,tmp.rs,orange.css}
+
+mkdir -p "$DATA_DIR/learning/globby/inner/inner"
+touch $DATA_DIR/learning/globby/inner/{stat.txt,service.html}
+echo -e "Done!\n"
