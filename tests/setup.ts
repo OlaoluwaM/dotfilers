ef3ff924c6fa15cd38fde2ae7d12309362f76bc5
Olaoluwa Mustapha <37044906+OlaoluwaM@users.noreply.github.com>
2022-11-13 11:18:04 +0100

    Revert "feat(release-major): v1 Release (#17)"
    This reverts commit a0cefc640bff6fcea947bb086fa2efec809394c8.

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,11 +1,1 @@
-import 'jest-extended';
-
-import path from 'path';
-
-import { jest } from '@jest/globals';
-import { getAbsolutePathsForFile } from '../src/constants';
-
-const { __dirname: __testDirname } = getAbsolutePathsForFile(import.meta.url);
-export const TEST_DATA_DIR_PREFIX = path.join(__testDirname, './test-data');
-
-jest.spyOn(process, 'exit').mockImplementation(() => undefined as never);
+export {};
