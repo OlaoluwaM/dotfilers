3105ce83fde7d4db1dc0daeaad7361221770899d
Olaoluwa Mustapha <37044906+OlaoluwaM@users.noreply.github.com>
2022-11-13 11:22:46 +0100

    fix(ci): Added fallback to native sed command in patch script (#19)

--- dist-patch.sh
+++ dist-patch.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
 
-if ! command -v sd &>/dev/null; then
-  echo "sd (https://github.com/chmln/sd) is required to run this script. Please install it"
-  exit 1
-fi
-
 set -o errexit
 
-# These commands patch up the imports of the compiled source to allow
-# these CJS modules to be accessed in an ESM evironment
 echo "Fixing up fp-ts, io-ts, and monocle-ts imports..."
-grep -rl ./dist -e "fp-ts" | xargs -I _ sd "\b(fp-ts(.*))\b" '$1.js' _
-grep -rl ./dist -e "io-ts" | xargs -I _ sd "\b(io-ts(.*))\b" '$1.js' _
-grep -rl ./dist -e "monocle-ts" | xargs -I _ sd "\b(monocle-ts(.*))\b" '$1.js' _
+
+if command -v sd &>/dev/null; then
+  # These commands patch up the imports of the compiled source to allow
+  # these CJS modules to be accessed in an ESM evironment
+  grep -rl ./dist -e "fp-ts" | xargs -I _ sd "\b(fp-ts(.*))\b" '$1.js' _
+  grep -rl ./dist -e "io-ts" | xargs -I _ sd "\b(io-ts(.*))\b" '$1.js' _
+  grep -rl ./dist -e "monocle-ts" | xargs -I _ sd "\b(monocle-ts(.*))\b" '$1.js' _
+else
+  echo "sd (https://github.com/chmln/sd) is not installed, switching to sed..."
+  grep -rl ./dist -e "fp-ts" | xargs -I _ sed -i -E "s#\b(fp-ts(.*))\b#\1.js#g" _
+  grep -rl ./dist -e "io-ts" | xargs -I _ sed -i -E "s#\b(io-ts(.*))\b#\1.js#g" _
+  grep -rl ./dist -e "monocle-ts" | xargs -I _ sed -i -E "s#\b(monocle-ts(.*))\b#\1.js#g" _
+fi
+
 echo "Done!"
